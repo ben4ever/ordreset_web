51923e75bf50fd21573c2020e77f31e4fe5fa3c7
Benjamin Dopplinger <b.dopplinger@gmail.com>
2017-09-26 16:45:27 +1000

    temp: Import `angular_components` one by one
    If I import `angular_components/angular_components.dart` `pub run
    angular_test` stalls without an error. Importing the required components
    one by one instead fixes this. Research what causes this and how to fix
    it.

--- lib/src/dropdown_component.dart
+++ lib/src/dropdown_component.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
-import 'package:angular_components/angular_components.dart';
+import 'package:angular_components/src/components/material_select/material_dropdown_select.dart';
+import 'package:angular_components/src/components/material_select/material_select_searchbox.dart';
+import 'package:angular_components/src/model/selection/selection_options.dart';
+import 'package:angular_components/src/model/selection/selection_model.dart';
+import 'package:angular_components/src/model/ui/has_renderer.dart';
 
 import 'api.dart';
 import 'application_tokens.dart';
@@ -13,8 +17,8 @@ abstract class DropdownComponent implements OnInit, HasDropdownType {
   OrderService _orderService;
   DropdownType _type;
 
-  @ViewChild(MaterialSelectSearchboxComponent)
-  MaterialSelectSearchboxComponent searchbox;
+  //@ViewChild(MaterialSelectSearchboxComponent)
+  //MaterialSelectSearchboxComponent searchbox;
 
   DropdownComponent(this._type, this._orderService) {
     options =
@@ -49,20 +53,21 @@ abstract class DropdownComponent implements OnInit, HasDropdownType {
 
   ItemRenderer<DropdownEntry> get itemRenderer => (de) => '$de';
 
-  void onDropdownVisibleChange(bool visible) {
-    if (visible) {
-      new Future<Null>(() {
-        searchbox.focus();
-      });
-    }
-  }
+  //void onDropdownVisibleChange(bool visible) {
+  //  if (visible) {
+  //    new Future<Null>(() {
+  //      searchbox.focus();
+  //    });
+  //  }
+  //}
 }
 
 @Component(
   selector: 'my-date-dropdown',
   templateUrl: 'dropdown_component.html',
   directives: const [
-    materialDirectives,
+    MaterialSelectSearchboxComponent,
+    MaterialDropdownSelectComponent,
   ],
 )
 class DateDropdownComponent extends DropdownComponent {
@@ -74,7 +79,8 @@ class DateDropdownComponent extends DropdownComponent {
   selector: 'my-procstatus-dropdown',
   templateUrl: 'dropdown_component.html',
   directives: const [
-    materialDirectives,
+    MaterialSelectSearchboxComponent,
+    MaterialDropdownSelectComponent,
   ],
 )
 class ProcStatusDropdownComponent extends DropdownComponent {
@@ -86,7 +92,8 @@ class ProcStatusDropdownComponent extends DropdownComponent {
   selector: 'my-procresult-dropdown',
   templateUrl: 'dropdown_component.html',
   directives: const [
-    materialDirectives,
+    MaterialSelectSearchboxComponent,
+    MaterialDropdownSelectComponent,
   ],
 )
 class ProcResultDropdownComponent extends DropdownComponent {
