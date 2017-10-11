b4f1c2a5ec4a8a8e97cc69470d6c1350450dd115
Benjamin Dopplinger <b.dopplinger@gmail.com>
2017-10-11 16:31:27 +1100

    test: First version of `OrderService` test

--- test/order_service.dart
+++ test/order_service.dart
@@ -12,91 +12,27 @@ import 'package:ordreset/src/application_tokens.dart';
 import 'package:ordreset/src/blocker.dart';
 import 'package:ordreset/src/order_component.dart';
 import 'package:ordreset/src/order.dart';
+import 'package:ordreset/src/order_service.dart';
 import 'package:ordreset/testing.dart';
 import 'order_po.dart';
 
 @AngularEntrypoint()
 void main() {
-  OrderPO po;
-  NgTestFixture<ParentComponent> fixture;
-  List<Request> requests;
-  Map<String, Blocker> blockers;
+  var blockers = {
+    'api': new Blocker(),
+  };
+
+  var serv = new Injector.slowReflective([
+    provide(blockApi, useValue: blockers['api'].block),
+    provide(BaseClient, useFactory: mockClientFactory),
+    provide(Api, useClass: Api, deps: [BaseClient]),
+    OrderService,
+  ]).get(OrderService);
 
   tearDown(disposeAnyRunningTest);
 
-  setUp(() async {
-    blockers = {
-      'api': new Blocker(),
-      'iconChange': new Blocker(),
-    };
-    requests = <Request>[];
-    final testBed = new NgTestBed<ParentComponent>().addProviders([
-      provide(blockApi, useValue: blockers['api'].block),
-      provide(blockIconChange, useValue: blockers['iconChange'].block),
-      provide(requestList, useValue: requests),
-      provide(BaseClient, useFactory: mockClientFactory),
-      provide(Api, useClass: Api, deps: [BaseClient]),
-      materialProviders,
-    ]);
-    fixture = await testBed.create();
-    po = await fixture.resolvePageObject(OrderPO);
-    await fixture.update();
-  });
-
-  test('check initial state', () async {
-    int idx = 0;
-    for (var val in [
-      '1',
-      '2017-01-02 04:08:16',
-      'partner1',
-      'msgtype1',
-      'procenv1',
-      'procdesc1',
-      'procmsg1',
-      'errmsg1'
-    ]) {
-      expect(await po.getTdText(idx++), val);
-    }
-    expect(await po.buttons, hasLength(3));
-  });
-
-  test('click "resubmit"', () async {
-    await po.clickButton(1);
-    await fixture.update();
-    expect(await po.spinners, hasLength(1));
-
+  test('use injected service', () async {
     blockers['api'].unblock();
-    await fixture.update();
-    expect(await po.iconsDone, hasLength(1));
-
-    blockers['iconChange'].unblock();
-    await fixture.update();
-    expect(await po.buttons, hasLength(3));
-    expect(requests, hasLength(1));
+    expect(serv, new isInstanceOf<OrderService>());
   });
-
-  test('click "cancel"', () async {
-    await po.clickButton(2);
-    await fixture.update();
-    expect(await po.spinners, hasLength(1));
-
-    blockers['api'].unblock();
-    await fixture.update();
-    expect(await po.iconsDone, hasLength(1));
-
-    blockers['iconChange'].unblock();
-    await fixture.update();
-    expect(await po.buttons, hasLength(3));
-    expect(requests, hasLength(1));
-  });
-}
-
-@Component(
-  selector: 'my-parent',
-  template: '<my-order [order]="order1"></my-order>',
-  directives: const [OrderComponent],
-)
-class ParentComponent {
-  Order get order1 => new Order(1, new DateTime(2017, 1, 2, 4, 8, 16),
-      'partner1', 'msgtype1', 'procenv1', 'procdesc1', 'procmsg1', 'errmsg1');
 }
